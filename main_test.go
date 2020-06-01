a8d81278e7d7e0a5482a43e255e29d9bda1a4aab
Gildas <gildas@breizh.org>
2020-06-01 15:39:34 +0900

    more logging

--- main_test.go
+++ main_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"io/ioutil"
 	"os"
+	"path/filepath"
 	"strings"
 	"testing"
 
@@ -19,9 +20,10 @@ func TestHasProperEnv(t *testing.T) {
 	assert.True(t, len(data) > 0)
 	filename, ok := os.LookupEnv("GOOGLE_CREDENTIALS")
 	assert.True(t, ok)
-	payload, err := ioutil.ReadFile(filename)
+	payload, err := ioutil.ReadFile(filepath.Join(".", filename))
 	assert.Nil(t, err)
 	assert.True(t, strings.Contains(string(payload), "END PRIVATE KEY"))
+	t.Logf("Filename=%s", filename)
 	t.Logf("Length=%d", len(string(payload)))
 	t.Logf("payload=%s", string(payload))
 }
