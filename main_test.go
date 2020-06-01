2cf7643eb67a0700f83ddcf374036fcdaeea40ae
Gildas <gildas@breizh.org>
2020-06-01 10:27:16 +0900

    more testing

--- main_test.go
+++ main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -8,4 +9,10 @@ import (
 
 func TestCanAdd(t *testing.T) {
 	assert.Equal(t, 4, add(2, 2))
+}
+
+func TestHasProperEnv(t *testing.T) {
+	data, ok := os.LookupEnv("GOOGLE_PROJECT_ID")
+	assert().True(t, ok)
+	assert().True(t, len(data) > 0)
 }
